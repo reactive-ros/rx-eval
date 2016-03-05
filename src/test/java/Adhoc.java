a2f5127fbbc9a6a4bfc4dd1ea85312dde27e1584
omelkonian <sdi1000128@di.uoa.gr>
2016-03-05 20:59:56 +0200

    Transfering

--- src/test/java/Adhoc.java
+++ src/test/java/Adhoc.java
@@ -1,4 +1,7 @@
 import org.junit.Test;
+import org.rhea_core.Stream;
+import org.rhea_core.distribution.Distributor;
+import rx_eval.RxjavaEvaluationStrategy;
 import test_data.utilities.Threads;
 
 /**
@@ -8,6 +11,10 @@ public class Adhoc {
 
     @Test
     public void test() {
+        Stream.configure(new Distributor(RxjavaEvaluationStrategy::new));
+
+        Stream.range(0, 20).subscribe(i -> System.out.println(i));
+
         Threads.sleep();
     }
 }
--- src/test/java/Tester.java
+++ src/test/java/Tester.java
@@ -1,14 +1,10 @@
 import org.junit.Test;
 import org.rhea_core.Stream;
 import org.rhea_core.distribution.Distributor;
-import org.rhea_core.distribution.Machine;
 import rx_eval.RxjavaEvaluationStrategy;
 import test_data.TestData;
 import test_data.TestInfo;
 import test_data.utilities.Colors;
-
-import java.util.ArrayList;
-import java.util.Collections;
 
 /**
  * @author Orestis Melkonian
@@ -16,9 +12,7 @@ import java.util.Collections;
 public class Tester {
     @Test
     public void single_threaded() {
-
-        Stream.configure(new RxjavaEvaluationStrategy(), new Distributor());
-
+        Stream.configure(new Distributor(RxjavaEvaluationStrategy::new));
         for (TestInfo test : TestData.tests()) {
             System.out.print(test.name + ": ");
             if (test.equality())
@@ -32,16 +26,14 @@ public class Tester {
 
     @Test
     public void multi_threaded() {
-
-        Stream.configure(new RxjavaEvaluationStrategy(), new Distributor());
-
+        Stream.configure(new Distributor(RxjavaEvaluationStrategy::new));
         for (TestInfo test : TestData.tests()) {
             System.out.print(test.name + ": ");
             if (test.equality())
                 Colors.println(Colors.GREEN, "Passed");
             else {
                 Colors.println(Colors.RED, "Failed");
-                System.out.println(test.q1 + " != " + test.q2);
+                System.out.println(test.q1 + "\n\t!=\n" + test.q2);
             }
         }
     }
